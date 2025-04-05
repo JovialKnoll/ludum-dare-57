5779bb54d8670f59dfae17118c86141d387d3ffe
Brett Kaplan <BrettCKaplan@gmail.com>
2025-04-05 16:36:04 -0400

    tweaking movement a lot

--- src/sprite/shot.py
+++ src/sprite/shot.py
@@ -10,14 +10,30 @@ class Shot(jovialengine.GameSprite):
     _COLLISION_MASK_LOCATION = constants.SHIP
     _COLLISION_MASK_ALPHA_OR_COLORKEY = constants.COLORKEY
 
+    _JERK = 0.001 * 0.0004
+    _INITIAL_ACCEL = 0.001 * -0.1
+    _MAX_ACCEL = 0.001 * 0.8
+    _INITIAL_SPEED = 0.001 * 120
+    _MAX_SPEED = 0.001 * 240
+
     __slots__ = (
         'angle',
+        '_accel',
+        '_speed',
     )
 
     def _start(self, mode):
         self.angle: float = mode.arrow_angle
+        self._accel = self._INITIAL_ACCEL
+        self._speed = self._INITIAL_SPEED
 
     def update(self, dt, camera):
-        distance = dt * 0.001 * 20
+        old_accel = self._accel
+        self._accel += dt * self._JERK
+        self._accel = min(self._accel, self._MAX_ACCEL)
+        distance = dt * self._speed + (old_accel + self._accel) * dt * dt / 4
+        self._speed += dt * self._accel
+        self._speed = min(self._speed, self._MAX_SPEED)
+        distance = min(distance, dt * self._MAX_SPEED)
         vec = utility.angle_vector(distance, self.angle)
         self.rect.move_ip(vec.x, vec.y)
