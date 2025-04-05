7f5c4e23de6b968188bbea9e0a20ff7e97c90dd1
Brett Kaplan <BrettCKaplan@gmail.com>
2025-04-05 12:44:37 -0400

    make a shot, slow down angle changes while there are shots

--- src/mode/modeplay.py
+++ src/mode/modeplay.py
@@ -2,8 +2,9 @@ import jovialengine
 import pygame
 
 import constants
+import sprite
 from .modescreensize import ModeScreenSize
-from sprite import Ship
+
 
 class ModePlay(ModeScreenSize):
     _HORIZON = 40
@@ -25,7 +26,7 @@ class ModePlay(ModeScreenSize):
             (0, self._HORIZON, constants.SCREEN_SIZE[0], constants.SCREEN_SIZE[1]))
         # should set up background more probably
 
-        self._ship = Ship(midbottom=(constants.SCREEN_SIZE[0] // 2, self._HORIZON))
+        self._ship = sprite.Ship(midbottom=(constants.SCREEN_SIZE[0] // 2, self._HORIZON))
         self._ship.start(self)
         self._shots = pygame.sprite.Group()
         self._draw_arrow = True
@@ -35,14 +36,18 @@ class ModePlay(ModeScreenSize):
     def _take_frame(self, input_frame):
         if input_frame.was_input_pressed(constants.EVENT_A):
             if len(self._shots) < self._MAX_SHOTS:
-                # create shot here
-                pass
-        pass
+                pos = self._get_aim_end(self._ship.rect.midbottom, 20)
+                shot = sprite.Shot(center=pos)
+                shot.start(self)
+                self._shots.add(shot)
 
     def _update_pre_sprites(self, dt):
         self._arrow_angle += dt * self._arrow_vel
         vel_change = 0
-        accel_amount = dt * self._ANGLE_BASIS * 2
+        angle_basis = self._ANGLE_BASIS
+        if self._shots:
+            angle_basis = self._ANGLE_BASIS / 2
+        accel_amount = dt * angle_basis * 2
         extra_clamp = None
         if self._input_frame.get_input_state(0, constants.EVENT_LEFT) == 1:
             vel_change -= accel_amount
@@ -61,11 +66,11 @@ class ModePlay(ModeScreenSize):
                     vel_change += accel_amount
                 if self._arrow_angle < 90:
                     vel_change -= accel_amount
-        if self._arrow_vel < -self._ANGLE_BASIS * 2 or self._arrow_vel > self._ANGLE_BASIS * 2:
+        if self._arrow_vel < -angle_basis * 2 or self._arrow_vel > angle_basis * 2:
             vel_change = 0
         old_vel = self._arrow_vel
         self._arrow_vel += vel_change
-        self._arrow_vel = jovialengine.utility.clamp(self._arrow_vel, -self._ANGLE_BASIS * 2, self._ANGLE_BASIS * 2)
+        self._arrow_vel = jovialengine.utility.clamp(self._arrow_vel, -angle_basis * 2, angle_basis * 2)
         self._arrow_angle += dt * (self._arrow_vel - old_vel) / 2
         if extra_clamp:
             self._arrow_angle = extra_clamp(self._arrow_angle, 90)
@@ -83,14 +88,14 @@ class ModePlay(ModeScreenSize):
     def _draw_post_sprites(self, screen, offset):
         if self._draw_arrow and self._ship.alive():
             start = pygame.Vector2(self._ship.rect.midbottom) + offset
-            end = self._get_aim_end(start)
+            end = self._get_aim_end(start, 30)
             pygame.draw.line(screen, "red", start, end)
 
     def _cleanup(self):
         self._shots.empty()
         del self._ship
 
-    def _get_aim_end(self, start: pygame.Vector2):
-        vec = pygame.Vector2(-30, 0)
+    def _get_aim_end(self, start: pygame.typing.Point, distance: int):
+        vec = pygame.Vector2(-distance, 0)
         vec.rotate_ip(-self._arrow_angle)
         return start + vec
